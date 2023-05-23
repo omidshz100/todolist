c868cbb0d5b1c4ba0a6c89491f3ceab49d170b16
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-05-23 11:58:43 +0200

    project is ready to use

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
 
 @main
 struct ToDoListApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ ToDoList/other/fb.swift
@@ -0,0 +1,36 @@
+//
+//  fb.swift
+//  ToDoList
+//
+//  Created by Omid Shojaeian Zanjani on 23/05/23.
+//
+ 
+
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
