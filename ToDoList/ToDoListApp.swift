0f01ae06f1d65231f9f63ea2aa3d0ccf106ae799
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-05-23 05:37:17 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Omid Shojaeian Zanjani on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
